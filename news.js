d582679397d8a24afbb6dbd620a8e90bf74951c4
Saurav017 <sauravabde@gmail.com>
2023-04-08 01:58:18 +0530

    finalupdate

--- news.js
+++ news.js
@@ -30,3 +30,17 @@ const displayNews = async () => {
         news.appendChild(newsItem);
     });
 }
+
+
+const icon = document.querySelector('.toggle-btn');
+
+
+// dark mode toggle
+icon.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    if (document.body.classList.contains('dark-theme')) {
+        icon.innerHTML = '<i class="fas fa-sun"></i>';
+    } else {
+        icon.innerHTML = '<i class="fas fa-moon"></i>';
+    }
+});
